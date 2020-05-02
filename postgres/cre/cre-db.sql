4b08a7a189f08923a869b634209f28f24d211d7b
Sam Matthews <sam.matthews.developer@gmail.com>
2020-05-03 10:21:12 +1200

    Create script for postgres database.

---
+++ postgres/cre/cre-db.sql
@@ -0,0 +1,14 @@
+/*
+    cre-db.sql
+    Sam Matthews
+    3rd May 2020.
+
+    CREATE BASE Databse, for project. Called TRADING-BOT.
+
+*/
+
+-- DROP DATABASE
+DROP DATABASE "trading-bot";
+
+--CREATE DATABASE
+CREATE DATABASE "trading-bot" OWNER "sam"; 
